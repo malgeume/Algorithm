a788b0a85fe7cd68f8ff7fb5d5bab4bee6977813
기현 <149554857+malgeume@users.noreply.github.com>
2025-01-03 11:08:12 +0900

    [Silver IV] Title: 국영수, Time: 396 ms, Memory: 80572 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,8 @@
+import sys
+N = int(sys.stdin.readline())
+students = []
+for i in range(N):
+    students.append(list(sys.stdin.readline().split()))
+students.sort(key = lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+for j in students:
+    print(j[0])
