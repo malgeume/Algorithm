dc62bbdafdc425b398fb18c5e3b9db2d0bfc7797
기현 <149554857+malgeume@users.noreply.github.com>
2025-01-03 11:14:34 +0900

    [Bronze I] Title: 단어 공부, Time: 68 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,10 @@
+word = input().upper()
+unique = list(set(word))
+arr = []
+for i in unique:
+    cnt = word.count(i)
+    arr.append(cnt)
+if arr.count(max(arr)) > 1:
+    print("?")
+else:
+    print(unique[arr.index(max(arr))])
