6f7dc58fe5b72c12ee27a207e80cc4ea70befd9f
기현 <149554857+malgeume@users.noreply.github.com>
2025-01-03 11:04:47 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,22 @@
+import sys
+
+def DFS(arr, i, visited):
+    global cnt
+    visited[i] = True
+    cnt += 1
+    for j in range(len(arr[i])):
+        if arr[i][j] == 1 and not visited[j]:
+            DFS(arr, j, visited)
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    M = int(sys.stdin.readline())
+    arr = [[0 for i in range(N + 1)] for j in range(N + 1)]
+    for i in range(M):
+        a, b = map(int, sys.stdin.readline().split())
+        arr[a][b] = 1
+        arr[b][a] = 1
+    visited = [False for k in range(N + 1)]
+    cnt = 0
+    DFS(arr, 1, visited)
+    print(cnt - 1)
