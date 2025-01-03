61b7a5eea1d6b072891e3867aa77e46b93ee890c
기현 <149554857+malgeume@users.noreply.github.com>
2025-01-03 11:14:05 +0900

    [Bronze I] Title: 2007년, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -0,0 +1,6 @@
+X, Y = map(int, input().split())
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+for i in range(X - 1):
+    Y += days[i]
+print(weekday[Y % 7])
