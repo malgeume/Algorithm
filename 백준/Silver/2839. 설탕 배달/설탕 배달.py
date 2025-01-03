5e86d84a8424970577c6fd237f752cd83bd5f29f
기현 <149554857+malgeume@users.noreply.github.com>
2025-01-03 11:16:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,13 @@
+N = int(input())
+count = 0
+while 1:
+    if N % 5 != 0:
+        N -= 3
+        count += 1
+    elif N % 5 == 0:
+        count += N // 5
+        print(count)
+        break
+    if N < 0:
+        print(-1)
+        break
