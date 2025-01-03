942dd6f6adf3ce974d7dad6f9d7d59839e0273d7
기현 <149554857+malgeume@users.noreply.github.com>
2025-01-03 11:04:42 +0900

    [Silver II] Title: 촌수계산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,31 @@
+import sys
+
+def DFS(p, cnt):
+    global flag
+    visited[p] = True
+    if p == b:
+        flag = True
+        result.append(cnt)
+        return
+    else:
+        for s in arr[p]:
+            if not visited[s]:
+                DFS(s, cnt + 1)
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    a, b = map(int, sys.stdin.readline().split())
+    m = int(sys.stdin.readline())
+    arr = [[] for i in range(n + 1)]
+    result = []
+    for i in range(m):
+        x, y = map(int, sys.stdin.readline().split())
+        arr[x].append(y)
+        arr[y].append(x)
+    visited = [False for k in range(n + 1)]
+    flag = False
+    DFS(a, 0)
+    if flag:
+        print(result[0])
+    else:
+        print(-1)
